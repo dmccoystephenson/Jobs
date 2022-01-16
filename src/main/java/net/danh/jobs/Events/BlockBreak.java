f0e02294b4e2c1b1455ccb54cbe97df205fe9c06
VoChiDanh <vochidanh.hs.c@gmail.com>
2022-01-16 14:07:54 +0700

    Đổi givemoney thành givexp
    Signed-off-by: VoChiDanh <vochidanh.hs.c@gmail.com>

--- src/main/java/net/danh/jobs/Events/BlockBreak.java
+++ src/main/java/net/danh/jobs/Events/BlockBreak.java
@@ -2,7 +2,6 @@ package net.danh.jobs.Events;
 
 import net.danh.jobs.Files.Files;
 import net.danh.jobs.Jobs;
-import net.danh.jobs.Manager.Manager;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
@@ -19,18 +18,18 @@ public class BlockBreak {
         Player p = e.getPlayer();
         if (Files.getInstance().getdata().getString("players." + p.getName()).equals("THOMO"))
             for (String key : Files.getInstance().getconfig().getConfigurationSection("miner_values").getKeys(false)) {
-                double value = Files.getInstance().getconfig().getDouble("miner_values." + key);
+                int value = Files.getInstance().getconfig().getInt("miner_values." + key);
                 if (b.getType().equals(Material.getMaterial(key))) {
-                    Manager.getInstance().payMoney(p, b, value);
+                    p.giveExp(value);
                     if (Files.getInstance().getconfig().getBoolean("debug"))
                         Jobs.getInstance().getLogger().log(Level.INFO, String.valueOf(p.getName()) + " (" + p.getUniqueId() + ") broke a block " + b.getType() + " located in " + b.getLocation().toString());
                 }
             }
         if (Files.getInstance().getdata().getString("players." + p.getName()).equals("TIEUPHU"))
             for (String key : Files.getInstance().getconfig().getConfigurationSection("treecutter_values").getKeys(false)) {
-                double value = Files.getInstance().getconfig().getDouble("treecutter_values." + key);
+                int value = Files.getInstance().getconfig().getInt("treecutter_values." + key);
                 if (b.getType().equals(Material.getMaterial(key))) {
-                    Manager.getInstance().payMoney(p, b, value);
+                    p.giveExp(value);
                     if (Files.getInstance().getconfig().getBoolean("debug"))
                         Jobs.getInstance().getLogger().log(Level.INFO, String.valueOf(p.getName()) + " (" + p.getUniqueId() + ") broke a block " + b.getType() + " located in " + b.getLocation().toString());
                 }
